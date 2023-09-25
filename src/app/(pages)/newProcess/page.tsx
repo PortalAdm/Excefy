2ec5ccf2d08d3c3613d6699843869e7c81b8a72b
Felipe <felipesullivan50@gmail.com>
2023-09-25 12:11:20 -0300

    feat: adiciona parcialmente a tabela de processos e fluxograma

---
+++ src/app/(pages)/newProcess/page.tsx
@@ -0,0 +1,9 @@
+import { BpmnView } from '~features/diagramView';
+
+export default function newProcesspage() {
+  return (
+    <div className="w-screen h-screen">
+      <BpmnView />
+    </div>
+  );
+}
