a818c1230dbf87feb10ddb2a40237e0cda0877ec
˜cesarvargasjr˜ <cesar_vargas_jr@hotmail.com>
2023-09-29 11:45:47 -0300

    Updates routes and pages

--- src/app/features/dashboard/views/ProcessList.tsx
+++ src/app/features/dashboard/views/ProcessList.tsx
@@ -1,15 +1,25 @@
 import { TableList } from '~shared/components/TableList';
 import { listHeaders, mocckedProcessContent } from '../dashboardUtils';
+import { Button } from '~/src/app/shared/components/Button';
 
 export function ProcessList() {
   return (
-    <TableList.root>
-      <TableList.header>
-        <TableList.name titles={listHeaders} />
-      </TableList.header>
-      <TableList.body>
-        <TableList.content content={mocckedProcessContent} />
-      </TableList.body>
-    </TableList.root>
+    <div className="p-20">
+      <Button.root size="small" className="mb-20">
+        <Button.link href={'/new-process'}>
+          <Button.contentWrapper>
+            <Button.label text="Criar processo" color="white" size="lg" weigth="bold" />
+          </Button.contentWrapper>
+        </Button.link>
+      </Button.root>
+      <TableList.root>
+        <TableList.header>
+          <TableList.name titles={listHeaders} />
+        </TableList.header>
+        <TableList.body>
+          <TableList.content content={mocckedProcessContent} />
+        </TableList.body>
+      </TableList.root>
+    </div>
   );
 }
