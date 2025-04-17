51065fec21551e21404c12c0c80447c5baa4fa66
Felipe Emanuel <felipesullivan50@gmail.com>
2025-04-17 15:36:07 -0300

    adiciona novos conectores à lista de templates

--- src/app/features/diagramView/.camunda/element-templates/index.ts
+++ src/app/features/diagramView/.camunda/element-templates/index.ts
@@ -1,4 +1,15 @@
 import restConnector from './rest-connector.json';
 import googleDriverConnnector from './google-driver-connector.json';
+import sqlConnector from './sql-connector.json';
+import twilioConnector from './twilio-connector.json';
+import chatGptConnector from './chat-gpt-connector.json';
+import emailConnector from './email-connector.json';
 
-export const templates = [restConnector, googleDriverConnnector];
+export const templates = [
+  restConnector,
+  googleDriverConnnector,
+  sqlConnector,
+  twilioConnector,
+  chatGptConnector,
+  emailConnector
+];
